f71fd17ada75bd8e461d993b50189abc22ba9056
PECMM <pecmmhnst@gmail.com>
2020-12-27 03:28:10 +0900

    ブイアパクリスマスビンゴカード 改善＆チェッカー実装
    * チェッカーの実装
    * 内部実装の修正

---
+++ vapartChristmas/bingo-vapartChristmas2020.js
@@ -0,0 +1,38 @@
+const bingo = {
+  maxNumber: 75,
+  rows: 5,
+  cols: 5,
+  freeCenter: true,
+  //
+  name: 'ブイアパクリスマス2020',
+  referenceHTML: '#ブイアパクリスマス2020 <a href="https://twitter.com/Uge_And/status/1341326316323315713">ビンゴ参加要項ツイート</a>',
+  imagePath: 'base.jpg',
+  imageStyle: {
+    height: '100%',
+    maxHeight: '1200px',
+  },
+  box: {
+    // ビンゴの欄が均等に並んでいる前提
+    firstLeft:  44,
+    firstTop : 268,
+    width    : 182,
+    height   : 190,
+    xStep    : 203,
+    yStep    : 209,
+  },
+  text: {
+    fontSize: 96,
+    // box内のオフセット
+    xOffset: 90,
+    yOffset: 96,
+  },
+  boxCheck: {
+    padding: 10,
+    lineWidth: 8,
+    //省略可 color
+    //省略可 alpha
+  },
+  debug: false,
+};
+
+export default bingo;
