d97091fbdf7e419055045b05f3db698a903e774c
PECMM <pecmmhnst@gmail.com>
2021-02-15 00:41:52 +0900

    774チャート ハニストバレンタイン2021妄想用のParamScriptを追加

---
+++ chart774/params-hnst2021vd.js
@@ -0,0 +1,48 @@
+const pageOptions = {
+  preset: {
+    autoStartName: '20210214-hnst2021vd',
+    restoreValueSet: [
+      { name: '20210214-hnst2021vd',
+        value: {
+          params:{
+            chartIconSize     : { min:100, max:100, },
+            backgroundFilename: 'img/hnst2021vd.jpg',
+          },
+          status:{ items:[
+            { charaName:'因幡はねる'      , },
+            { charaName:'宗谷いちか'      , },
+            { charaName:'日ノ隈らん'      , },
+            { charaName:'風見くく'        , },
+            { charaName:'柚原いづみ'      , },
+            { charaName:'白宮みみ'        , },
+            { charaName:'羽柴なつみ'      , },
+            { charaName:'瀬島るい'        , },
+            { charaName:'飛良ひかり'      , },
+            { charaName:'月野木ちろる'    , },
+            { charaName:'大浦るかこ'      , },
+            { charaName:'湖南みあ'        , },
+            { charaName:'龍ヶ崎リン'      , },
+            { charaName:'虎城アンナ'      , },
+            { charaName:'獅子王クリス'    , },
+            { charaName:'杏戸ゆげ'        , },
+            { charaName:'花奏かのん'      , },
+            { charaName:'季咲あんこ'      , },
+            { charaName:'不磨わっと'      , },
+            { charaName:'小森めと'        , },
+          ],}
+        },
+      },
+    ],
+  },
+  
+  contents: {
+    title: 'ハニストバレンタインのいけない妄想',
+    referenceHTML: "<a href='https://www.youtube.com/watch?v=kWy7sOjuaao&t=44m50s'>【ハニストバレンタイン】赤面待ったなし？！私たちと過ごす熱い夜！！ハニスト全員コラボ　Valentine's Day Special【周防パトラ / ハニスト</a>",
+    
+    roundOffMemberIcon: {
+      isRequired     : false,
+    },
+  },
+};
+
+export { pageOptions };
