11947a43e5df2e23192cda9a33261450569b5980
PECMM <pecmmhnst@gmail.com>
2021-02-18 01:51:59 +0900

    774チャート リンせし花より男子キャスト妄想用のParamScriptを追加

---
+++ chart774/params-20210217rinseshi.js
@@ -0,0 +1,52 @@
+const pageOptions = {
+  preset: {
+    autoStartName: '20210217-rinseshi',
+    restoreValueSet: [
+      { name: '20210217-rinseshi',
+        value: {
+          params:{
+            chartIconSize     : { min:100, max:100, },
+            backgroundFilename: 'img/20210217-rinseshi.jpg',
+          },
+          status:{ items:[
+            { charaName:'因幡はねる'      , },
+            { charaName:'宗谷いちか'      , },
+            { charaName:'日ノ隈らん'      , },
+            { charaName:'風見くく'        , },
+            { charaName:'柚原いづみ'      , },
+            { charaName:'白宮みみ'        , },
+            { charaName:'羽柴なつみ'      , },
+            { charaName:'瀬島るい'        , },
+            { charaName:'飛良ひかり'      , },
+            { charaName:'月野木ちろる'    , },
+            { charaName:'大浦るかこ'      , },
+            { charaName:'湖南みあ'        , },
+            { charaName:'西園寺メアリ'    , },
+            { charaName:'島村シャルロット', },
+            { charaName:'周防パトラ'      , },
+            { charaName:'堰代ミコ'        , },
+            { charaName:'龍ヶ崎リン'      , },
+            { charaName:'虎城アンナ'      , },
+            { charaName:'獅子王クリス'    , },
+            { charaName:'杏戸ゆげ'        , },
+            { charaName:'花奏かのん'      , },
+            { charaName:'季咲あんこ'      , },
+            { charaName:'不磨わっと'      , },
+            { charaName:'小森めと'        , },
+          ],}
+        },
+      },
+    ],
+  },
+  
+  contents: {
+    title: 'もしも774incメンバーが『花より男子』のキャストだったら',
+    referenceHTML: "<a href='https://www.youtube.com/watch?v=0K-x4Kk0lx0'>【#リンせし​】この２人…もしかして似てる？！【瀬島るい / あにまーれ】</a>",
+    
+    roundOffMemberIcon: {
+      isRequired     : false,
+    },
+  },
+};
+
+export { pageOptions };
